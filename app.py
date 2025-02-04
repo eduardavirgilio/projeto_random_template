549e5b31c442bc7b3fba9c7349c6d958e194cb8c
Eduarda Virgilio <eduarda.virgilio2630@gmail.com>
2025-02-04 09:01:58 -0300

    imagens e frases aleatorias funcionando

--- app.py
+++ app.py
@@ -13,20 +13,34 @@ app = Flask(__name__)
 
 lista_cores = ["lavanderblush", "pink", "papayawhip", "snow", "powderblue", "#BABACA", "#e1c8fd"]
 
+lista_imagens = ["taylor_ttpd.jpg", "taylor-swift-ttpd-album-review.png", "taylor-lencol-ttpd.jpg", "clipe-fortnight.jpg", "taylor-malone.jpg", "taylor-shot-ttpd.jpg"]
+
+letras_musica = ["'E quando eu senti como se eu fosse um cardigã velho, debaixo da cama de alguém, você me vestiu e disse que eu era seu favorito' (Cardigan)", 
+                 "'Todas as minhas manhãs são segundas-feiras' (Fortnight)", 
+                 "'Você sabe, os melhores filmes de todos os tempos nunca foram feitos' (The 1)", 
+                 "'Eu olho direto para o Sol, mas nunca para o espelho.' (Anti-Hero)", 
+                 "'Você não sabe o que tem até que tenha perdido' (You're losing me)", 
+                 "'Eu estou tão cansada de correr o mais rápido que consigo, imaginando se eu chegaria lá mais rápido se eu fosse um homem' (The man)", 
+                 "'E se você nunca sangrar, você nunca vai crescer' (The 1)"]
+
 @app.route("/sobre")
 def pagina_sobre():
     cor_fundo = random.choice(lista_cores)
     return render_template("sobre.html", cor_fundo_html = cor_fundo)
 
-
-lista_imagens = ["taylor_ttpd.jpg", "taylor_swift.png"]
-letras_musica = ["'E quando eu senti como se eu fosse um cardigã velho, debaixo da cama de alguém, você me vestiu e disse que eu era seu favorito' (Cardigan)", "'Todas as minhas manhãs são segundas-feiras' (Fortnight)"]
-
 @app.route("/home")
-def pagina_inicial():
-    lista_imagens = random.choice(lista_imagens)
-    letras_musica = random.choice(letras_musica)
-    return render_template("home.html", lista_imagens_html = lista_imagens, letras_musica_html = letras_musica)
+
+def pagina_principal():
+    imagem_aleatoria = random.choice(lista_imagens)
+
+    frase_aleatoria = random.choice(letras_musica)
+
+    return render_template("home.html", 
+                           frase_aleatoria_html = frase_aleatoria, 
+                           imagem_aleatoria_html = imagem_aleatoria)
+
+
+# lista_imagens_html = lista_imagens, 
 
 #-----------------------------------------------------------------------------
 
